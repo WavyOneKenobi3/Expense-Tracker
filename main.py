fa7fc1441e664b543a0890d7363037de2af81f07
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-05 21:11:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,7 @@
 
 #row with names of expenses(string), row for interger(number), total for all expenses (number add together)
 
-  
+  #expense tracker
+
+# An app that tracks and categorizes expenses, storing data in a CSV or SQLite database.
+# Skills: File handling or databases, data analysis
