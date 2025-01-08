92a15c59a3bd87f59fbda05a888ea6bba6bfd323
Mark <19152780+WavyOneKenobi3@users.noreply.github.com>
2025-01-08 14:27:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,31 +7,37 @@
 # An app that tracks and categorizes expenses, storing data in a CSV or SQLite database.
 # Skills: File handling or databases, data analysis
 
-list_expense = []
+list_expense = {}
 
 
 def add_expense():
-  users = input("Enter your expense here. ").lower()
-  list_expense.append(users)
+  key = input("Enter your expense here. ").lower()
+  value = input("Amount of the expense")
+  list_expense.update({key: value})
+  
   
   
   
 def Delete_expense():
-  users = input("Which expense do you want to delete? ").lower()
-  if users in list_expense:
-    list_expense.pop(users)
+  delete_bill = input("which expense do you want to delete? ").lower()
+  if delete_bill in list_expense:
+    del list_expense[delete_bill]
   else:
     print("Expense not found")
     
     
-while True:  
-  print("Expense Tracker")
-  users = int(input("1.Add expense \n2. Delete expense \n3. Show List of expenses "))
-  if users == 1:
-    add_expense()
-  elif users == 2:
-    Delete_expense()
-  elif users == 3:
-    print(list_expense)
-  
+while True:
+  try:  
+    print("Expense Tracker")
+    users = int(input("1.Add expense \n2. Delete expense \n3. Show List of expenses \n "))
+    if users == 1:
+      add_expense()
+    elif users == 2:
+      Delete_expense()
+    elif users == 3:
+      print(list_expense)
+    else:
+      print("Your selection is not an option")
+  except ValueError:
+    print("Invalid entry")
   
